2873bef893d4bfc95072170394d65603be0cdd2d
Francesco Disperati <francesco@cs.ucsb.edu>
2016-06-03 16:40:34 -0700

    Package setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='common_utils',
+    version='0.0.1',
+    packages=['simple_logging'],
+    description='Common utilities needed for testing, PCAP parsing and stuff.',
+    url='https://git.seclab.cs.ucsb.edu/cgc/common_utils',
+)
