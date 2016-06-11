2dacb033d01c2ae94193027df0ff26f0ede06531
Aravind, Machiry <machiry@cs.ucsb.edu>
2016-06-11 14:32:00 -0700

    Adding more packages

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from setuptools import setup
 setup(
     name='common_utils',
     version='0.0.1',
-    packages=['common_utils'],
+    packages=['common_utils', 'common_utils.binary_tester', 'common_utils.pcap_parser', 'common_utils.poll_sanitizer',
+              'common_utils.simple_logging'],
     description='Common utilities needed for testing, PCAP parsing and stuff.',
     url='https://git.seclab.cs.ucsb.edu/cgc/common_utils',
 )
