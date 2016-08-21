54672db02cb85d283f82cea9e4a9a62361eb73c8
Machiry Aravind Kumar <makrvcs@gmail.com>
2016-08-20 21:09:44 -0700

    Update setup.py
    Changing repo url

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(
     packages=['common_utils', 'common_utils.binary_tester', 'common_utils.pcap_parser', 'common_utils.poll_sanitizer',
               'common_utils.simple_logging'],
     description='Common utilities needed for testing, PCAP parsing and stuff.',
-    url='https://git.seclab.cs.ucsb.edu/cgc/common_utils',
+    url='https://github.com/mechaphish/common-utils',
 )
